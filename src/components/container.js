6198289cbef9386955ed13ebe642ce156ee26c05
Valerie Russell <vruss14@gmail.com>
2022-01-05 16:37:32 -0500

    update & simplify file structure

---
+++ src/components/container.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Container(props) {
+
+
+  return (
+    <main>
+        <div>
+            <h1>Placeholder Title</h1>
+        </div>
+    </main>
+  );
+}
+
+export default Container;
